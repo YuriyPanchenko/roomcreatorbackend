2106a284662d9850b04fcae60af0e966840685cd
Yuriy <yuriy.panchenko.2000@gmail.com>
2020-08-02 15:09:35 +0300

    add Point model

---
+++ src/main/java/yuriy/roomcreator/models/Point.java
@@ -0,0 +1,13 @@
+package yuriy.roomcreator.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Point {
+    private double xValue;
+    private double yValue;
+}
