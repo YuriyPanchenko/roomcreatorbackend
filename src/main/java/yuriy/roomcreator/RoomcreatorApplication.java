5b615b26c13b37e24be16226daa16525a929d92b
Yuriy <yuriy.panchenko.2000@gmail.com>
2020-08-02 15:11:33 +0300

    main class

---
+++ src/main/java/yuriy/roomcreator/RoomcreatorApplication.java
@@ -0,0 +1,13 @@
+package yuriy.roomcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomcreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomcreatorApplication.class, args);
+    }
+
+}
